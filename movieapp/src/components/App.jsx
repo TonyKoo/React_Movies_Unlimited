19e68bee2d7fad1b75e220a48f75adb2cd14ab71
Tony Koo <TonyKoo@msn.com>
2019-12-28 07:40:24 +0000

    test Ubuntu

--- movieapp/src/components/App.jsx
+++ movieapp/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navigation from "./Navigation";
 
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function App() {
   return (
